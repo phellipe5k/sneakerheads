cd38cd0fef1a77184c7435810adf6d2aef177370
Luiz Phellipe <phellipe501@gmail.com>
2020-12-19 01:57:11 -0300

    fixing route issues

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,11 +1,16 @@
+import Link from 'next/link';
 import Image from 'next/image'
 import * as S from './style';
 import PropTypes from 'prop-types';
 
 const Logo = ({ size }) => (
-  <S.Wrapper>
-    <S.Image src="logo.png" size={ size } />
-  </S.Wrapper>
+  <Link href="/">
+    <a>
+      <S.Wrapper>
+        <S.Image src="logo.png" size={ size } />
+      </S.Wrapper>
+    </a>
+  </Link>
 )
 
 
